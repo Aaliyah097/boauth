5dc8855a143900886b41487765db51d4ec614354
Aaliyah097 <name.boltz@gmail.com>
2024-03-11 17:11:41 +0300

    шаблоны сообщений ответов

--- handlers.py
+++ handlers.py
@@ -3,9 +3,12 @@ from aiogram import Router
 from aiogram.filters import Command, CommandObject
 from aiogram.types import Message, InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.enums import ParseMode
 from utils import get_start_params, verify_login, make_nonce, store_telegram_id
 from api_requests import get_account, activate_account
 from exceptions import UserNotFoundError
+import vars
+
 
 router = Router()
 
@@ -21,24 +24,24 @@ async def start_handler(msg: Message, command: CommandObject):
     redirect_uri = await get_start_params(command.args)
 
     if not login:
-        await msg.answer("Пожалуйста, установите имя пользователя в настройках телеграм")
+        await msg.answer(vars.LOGIN_NOT_FOUND_MESSAGE, parse_mode=ParseMode.HTML)
         return
 
     if not redirect_uri:
-        await msg.answer("Не найден код для входа")
+        await msg.answer(vars.START_CODE_NOT_FOUND, parse_mode=ParseMode.HTML)
         return
 
     try:
         account = await get_account(verify_login(login))
     except UserNotFoundError:
-        await msg.answer("Пользователь не найден")
+        await msg.answer(vars.USER_NOT_FOUND, parse_mode=ParseMode.HTML)
         return
 
     if not account.is_active:
         try:
             await activate_account(account.pk, telegram_id)
         except UserNotFoundError:
-            await msg.answer("Приглашение не найдено")
+            await msg.answer(vars.USER_NOT_FOUND, parse_mode=ParseMode.HTML)
             return
 
     nonce = await store_telegram_id(make_nonce(), telegram_id)
@@ -48,6 +51,6 @@ async def start_handler(msg: Message, command: CommandObject):
         text="Вернуться в приложение", url=str(redirect_uri) + f"?nonce={nonce}")
     )
 
-    await msg.answer(f"Привет, {first_name}!\n"
-                     f"Вход выполнен успешно, нажми на кнопку ниже, чтобы вернуться в приложение: {msg.from_user.id}",
-                     reply_markup=builder.as_markup())
+    await msg.answer(vars.AUTH_SUCCESS % (first_name, nonce),
+                     reply_markup=builder.as_markup(),
+                     parse_mode=ParseMode.HTML)
