cb9b0c44b8ccdfc73235c5aa3021308427c06704
Aaliyah097 <name.boltz@gmail.com>
2024-07-21 08:37:56 +0300

    все одноразовые коды приведены к одной длине 6

--- src/auth.py
+++ src/auth.py
@@ -72,7 +72,7 @@ class AuthorizationMiddleware(BaseMiddleware):
 
             if not account.partial_signup:
                 builder = ReplyKeyboardBuilder()
-                builder.row(buttons.SignupFinishButton(await store_telegram_id(make_nonce(16), telegram_id)))
+                builder.row(buttons.SignupFinishButton(await store_telegram_id(make_nonce(6), telegram_id)))
                 return await event.answer(
                     vars.ONBOARDING_REQUIRED,
                     reply_markup=builder.as_markup(resize_keyboard=True),
